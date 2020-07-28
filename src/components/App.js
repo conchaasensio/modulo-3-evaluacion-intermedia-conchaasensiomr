9206d61f37fc7b2037fa0f847e3c39196f3908ea
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-28 08:45:15 +0200

    Innitial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React /*, { useState }*/ from 'react';
+// import '../stylesheets/App.css'; Para importar el CSS.
+
+const App = () => {
+  // const [dato, funcionDato] = useState(); Para definir el estado inicial
+
+  return <div className="App">Hola, mundo.</div>;
+};
+
+export default App;
